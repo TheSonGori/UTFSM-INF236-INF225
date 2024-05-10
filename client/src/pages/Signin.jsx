ca1e1ab172e23a965e7a579bae71ded4e78d3121
Javiera Gutiérrez <92240920+TheSonGori@users.noreply.github.com>
2024-05-10 01:31:21 -0400

    The Last Change

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -111,19 +111,7 @@ function Signin() {
                 </div>
                 {/* Warning */}
                 <div className="mt-5">
-                  <div className="bg-amber-100 dark:bg-amber-400/30 text-amber-600 dark:text-amber-400 px-3 py-2 rounded">
-                    <svg
-                      className="inline w-3 h-3 shrink-0 fill-current mr-2"
-                      viewBox="0 0 12 12"
-                    >
-                      <path
-                        d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z"
-                      />
-                    </svg>
-                    <span className="text-sm">
-                      Para apoyarte durante la pandemia, las super funciones pro son gratis hasta el 31 de marzo.
-                    </span>
-                  </div>
+                  
                 </div>
               </div>
             </div>
