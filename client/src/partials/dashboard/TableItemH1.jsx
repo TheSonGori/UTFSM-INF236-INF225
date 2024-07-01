20809ab6f753f64411cacd0f81c632b2ef12d21a
SpicyMip <martin.pino@sansano.usm.cl>
2024-07-01 00:52:15 -0400

    xd

--- client/src/partials/dashboard/TableItemH1.jsx
+++ client/src/partials/dashboard/TableItemH1.jsx
@@ -16,15 +16,12 @@ function TableItemH1({ id, name, date, status, handleOpenModal }) {
       </td>
       <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
         <div className="flex items-center">
-          <label className="inline-flex">
-            <span className="sr-only">Select</span>
-            <button 
-              className="text-xs inline-flex font-medium rounded-full text-center px-2.5 py-1 bg-slate-100 dark:bg-slate-800 text-slate-500 dark:text-slate-400 transition-colors duration-300 hover:bg-emerald-100 hover:text-emerald-600 dark:hover:bg-emerald-400/30 dark:hover:text-emerald-400"
-              onClick={() => handleOpenModal({ id, name, date, status })} // Llamar con todo el objeto
-            >
-              Reserva
-            </button>
-          </label>
+          <button 
+            className="text-xs inline-flex font-medium rounded-full text-center px-2.5 py-1 bg-slate-100 dark:bg-slate-800 text-slate-500 dark:text-slate-400 transition-colors duration-300 hover:bg-emerald-100 hover:text-emerald-600 dark:hover:bg-emerald-400/30 dark:hover:text-emerald-400"
+            onClick={() => handleOpenModal({ id, name, date, status })} // Llamar con todo el objeto
+          >
+            Reserva
+          </button>
         </div>
       </td>
     </tr>
